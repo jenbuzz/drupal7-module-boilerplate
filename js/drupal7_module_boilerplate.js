4476e7ec2392a0fa29631d1a129249d2244775da
dan-lyn <daniel_lynge@hotmail.com>
2014-04-27 14:34:02 +0200

    added default Drupal.behaviors starter code

--- js/drupal7_module_boilerplate.js
+++ js/drupal7_module_boilerplate.js
@@ -1,1 +1,11 @@
-// TODO
+(function ($, Drupal, window, document, undefined) {
+
+  Drupal.behaviors.my_custom_behavior = {
+    attach: function(context, settings) {
+
+      // js code goes here!
+
+    }
+  };
+
+})(jQuery, Drupal, this, this.document);
